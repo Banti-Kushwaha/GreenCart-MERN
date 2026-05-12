2fb642f2ca051d7e2fb36852371fde8ae1623db3
Banti Kushwaha <bantihzb987@gmail.com>
2026-05-12 22:31:34 +0530

    Fixed Vercel serverless crash

--- server/server.js
+++ server/server.js
@@ -43,6 +43,4 @@ app.use('/api/cart', cartRouter);
 app.use('/api/address', addressRouter);
 app.use('/api/order', orderRouter);
 
-app.listen(port, ()=>{
-    console.log(`Server is running on http://localhost:${port}`);
-})
+export default app;
