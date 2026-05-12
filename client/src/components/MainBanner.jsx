95b0d11a099a6acd3c68231af605428141dd8aca
Banti Kushwaha <bantihzb987@gmail.com>
2026-05-12 20:13:00 +0530

    updated project

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -17,7 +17,7 @@ const MainBanner = () => {
       />
       <div className="absolute inset-0 flex flex-col items-center md:items-start justify-end md:justify-center pb-24 md:pd-0 px-4 md:pl-18 lg:pl-24">
         <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15">
-          Freshness You Can Trust, Savings You Will Love!
+          Fresh Groceries Delivered Straight From Farm To Your Doorstep
         </h1>
 
         <div className="flex items-center mt-6 font-medium">
