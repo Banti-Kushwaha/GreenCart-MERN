130b0644a474e951df2647e12825d4cd653aa3ce
Banti Kushwaha <bantihzb987@gmail.com>
2026-05-15 00:52:28 +0530

    fixed

--- server/middleware/authSeller.js
+++ server/middleware/authSeller.js
@@ -1,37 +1,41 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
 const authSeller = async (req, res, next) => {
-  const { sellerToken } = req.cookies;
 
-  if (!sellerToken) {
-    return res.json({
-      success: false,
-      message: "Not Authorized Seller",
-    });
-  }
+    const { sellerToken } = req.cookies;
 
-  try {
-    const tokenDecode = jwt.verify(
-      sellerToken,
-      process.env.JWT_SECRET
-    );
-
-    if (tokenDecode?.email === process.env.SELLER_EMAIL) {
-      next();
-    } else {
-      return res.json({
-        success: false,
-        message: "Not Authorized Seller",
-      });
+    if (!sellerToken) {
+        return res.json({
+            success: false,
+            message: 'Not Authorized Seller'
+        });
     }
-  } catch (error) {
-    console.log(error.message);
 
-    return res.json({
-      success: false,
-      message: error.message,
-    });
-  }
-};
+    try {
+
+        const tokenDecode = jwt.verify(
+            sellerToken,
+            process.env.JWT_SECRET
+        );
+
+        if (tokenDecode.email === process.env.SELLER_EMAIL) {
+            next();
+        } else {
+            return res.json({
+                success: false,
+                message: 'Not Authorized Seller'
+            });
+        }
+
+    } catch (error) {
+
+        console.log(error.message);
+
+        return res.json({
+            success: false,
+            message: error.message
+        });
+    }
+}
 
 export default authSeller;
