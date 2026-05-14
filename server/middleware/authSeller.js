f882260de208342847af9e9f9ec393e9a22b8c4f
Banti Kushwaha <bantihzb987@gmail.com>
2026-05-15 00:40:40 +0530

    fixed

--- server/middleware/authSeller.js
+++ server/middleware/authSeller.js
@@ -1,41 +1,37 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const authSeller = async (req, res, next) => {
+  const { sellerToken } = req.cookies;
 
-    const { sellerToken } = req.cookies;
+  if (!sellerToken) {
+    return res.json({
+      success: false,
+      message: "Not Authorized Seller",
+    });
+  }
 
-    if (!sellerToken) {
-        return res.json({
-            success: false,
-            message: 'Not Authorized Seller'
-        });
+  try {
+    const tokenDecode = jwt.verify(
+      sellerToken,
+      process.env.JWT_SECRET
+    );
+
+    if (tokenDecode?.email === process.env.SELLER_EMAIL) {
+      next();
+    } else {
+      return res.json({
+        success: false,
+        message: "Not Authorized Seller",
+      });
     }
+  } catch (error) {
+    console.log(error.message);
 
-    try {
-
-        const tokenDecode = jwt.verify(
-            sellerToken,
-            process.env.JWT_SECRET
-        );
-
-        if (tokenDecode.email === process.env.SELLER_EMAIL) {
-            next();
-        } else {
-            return res.json({
-                success: false,
-                message: 'Not Authorized Seller'
-            });
-        }
-
-    } catch (error) {
-
-        console.log(error.message);
-
-        return res.json({
-            success: false,
-            message: error.message
-        });
-    }
-}
+    return res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 export default authSeller;
--- server/middleware/authUser.js
+++ server/middleware/authUser.js
@@ -1,37 +1,39 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const authUser = (req, res, next) => {
-    const { token } = req.cookies;
+  const { token } = req.cookies;
 
-    if (!token) {
-        return res.json({
-            success: false,
-            message: 'Not Authorized User'
-        });
+  if (!token) {
+    return res.json({
+      success: false,
+      message: "Not Authorized User",
+    });
+  }
+
+  try {
+    const tokenDecode = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    );
+
+    if (tokenDecode?.id) {
+      req.userId = tokenDecode.id;
+      next();
+    } else {
+      return res.json({
+        success: false,
+        message: "Not Authorized User",
+      });
     }
 
-    try {
-        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    console.log(error.message);
 
-        if (tokenDecode.id) {
-            req.userId = tokenDecode.id;
-        } else {
-            return res.json({
-                success: false,
-                message: 'Not Authorized User'
-            });
-        }
-
-        next();
-
-    } catch (error) {
-        console.log(error.message);
-
-        return res.json({
-            success: false,
-            message: error.message
-        });
-    }
+    return res.json({
+      success: false,
+      message: "Invalid Token",
+    });
+  }
 };
 
 export default authUser;
